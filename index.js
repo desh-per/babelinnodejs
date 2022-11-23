b6bc838fd70dbd17e4f2001b7951184d94cd6e95
desh-per <113818716+desh-per@users.noreply.github.com>
2022-11-23 11:11:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello from the server');
 }).listen(4001);
 
-console.log('Server is up and running test1');
+console.log('Server is up and running with jenkin build');
 
 export default server;
