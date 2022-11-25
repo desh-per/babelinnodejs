bdbcb1a3b001c7723e73db1527d63d143cc8733a
desh-per <113818716+desh-per@users.noreply.github.com>
2022-11-25 15:19:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello from the server');
 }).listen(4001);
 
-console.log('Server is up and running with jenkin build 32');
+console.l('Server is up and running with jenkin build 32');
 
 export default server;
